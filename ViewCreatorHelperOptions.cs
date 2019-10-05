ba17a23e38cf40068beaebfe46854616e85b4fc5
alchemistmatt <matthew.monroe@pnnl.gov>
2019-10-04 17:03:31 -0700

    Update the program date

--- ViewCreatorHelperOptions.cs
+++ ViewCreatorHelperOptions.cs
@@ -9,7 +9,7 @@ namespace PgSqlViewCreatorHelper
         /// <summary>
         /// Program date
         /// </summary>
-        public const string PROGRAM_DATE = "October 3, 2019";
+        public const string PROGRAM_DATE = "October 4, 2019";
 
         #region "Properties"
 
