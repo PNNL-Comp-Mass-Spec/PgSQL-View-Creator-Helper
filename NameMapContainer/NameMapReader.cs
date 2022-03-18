36908cdae94e2720c4c3a4d3cdd20833c61b38d3
alchemistmatt <matthew.monroe@pnnl.gov>
2022-03-18 14:25:11 -0700

    Convert to using declarations

--- NameMapContainer/NameMapReader.cs
+++ NameMapContainer/NameMapReader.cs
@@ -40,75 +40,74 @@ namespace TableColumnNameMapContainer
 
             try
             {
-                using (var reader = new StreamReader(new FileStream(mapFile.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+                using var reader = new StreamReader(new FileStream(mapFile.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+
+                while (!reader.EndOfStream)
                 {
-                    while (!reader.EndOfStream)
+                    var dataLine = reader.ReadLine();
+                    linesRead++;
+
+                    if (string.IsNullOrWhiteSpace(dataLine))
+                        continue;
+
+                    var lineParts = dataLine.Split('\t');
+
+                    if (lineParts.Length < 5)
+                        continue;
+
+                    if (linesRead == 1 &&
+                        lineParts[0].Equals("SourceTable", StringComparison.OrdinalIgnoreCase) &&
+                        lineParts[1].Equals("SourceName", StringComparison.OrdinalIgnoreCase))
                     {
-                        var dataLine = reader.ReadLine();
-                        linesRead++;
+                        // Header line; skip it
+                        continue;
+                    }
 
-                        if (string.IsNullOrWhiteSpace(dataLine))
-                            continue;
+                    var sourceTableName = lineParts[0];
+                    var sourceColumnName = lineParts[1];
+                    var newSchema = lineParts[2];
+                    var newTableName = PossiblyUnquote(lineParts[3]);
+                    var newColumnName = PossiblyUnquote(lineParts[4]);
 
-                        var lineParts = dataLine.Split('\t');
+                    if (!tableNameMap.ContainsKey(sourceTableName))
+                    {
+                        string newSchemaToUse;
+                        if (string.IsNullOrEmpty(newSchema))
+                            newSchemaToUse = defaultSchema;
+                        else
+                            newSchemaToUse = newSchema;
 
-                        if (lineParts.Length < 5)
-                            continue;
+                        var replacer = new WordReplacer(sourceTableName, newTableName, newSchemaToUse);
+                        tableNameMap.Add(sourceTableName, replacer);
+                    }
 
-                        if (linesRead == 1 &&
-                            lineParts[0].Equals("SourceTable", StringComparison.OrdinalIgnoreCase) &&
-                            lineParts[1].Equals("SourceName", StringComparison.OrdinalIgnoreCase))
-                        {
-                            // Header line; skip it
-                            continue;
-                        }
+                    if (!columnNameMap.TryGetValue(newTableName, out var targetTableColumnMap))
+                    {
+                        targetTableColumnMap = new Dictionary<string, WordReplacer>(StringComparer.OrdinalIgnoreCase);
+                        columnNameMap.Add(newTableName, targetTableColumnMap);
+                    }
 
-                        var sourceTableName = lineParts[0];
-                        var sourceColumnName = lineParts[1];
-                        var newSchema = lineParts[2];
-                        var newTableName = PossiblyUnquote(lineParts[3]);
-                        var newColumnName = PossiblyUnquote(lineParts[4]);
+                    if (targetTableColumnMap.ContainsKey(sourceColumnName))
+                    {
+                        OnWarningEvent(
+                            "In file {0}, table {1} has multiple columns with the same source name, {2}",
+                            mapFile.Name, newTableName, sourceColumnName);
 
-                        if (!tableNameMap.ContainsKey(sourceTableName))
-                        {
-                            string newSchemaToUse;
-                            if (string.IsNullOrEmpty(newSchema))
-                                newSchemaToUse = defaultSchema;
-                            else
-                                newSchemaToUse = newSchema;
+                        continue;
+                    }
 
-                            var replacer = new WordReplacer(sourceTableName, newTableName, newSchemaToUse);
-                            tableNameMap.Add(sourceTableName, replacer);
-                        }
-
-                        if (!columnNameMap.TryGetValue(newTableName, out var targetTableColumnMap))
-                        {
-                            targetTableColumnMap = new Dictionary<string, WordReplacer>(StringComparer.OrdinalIgnoreCase);
-                            columnNameMap.Add(newTableName, targetTableColumnMap);
-                        }
-
-                        if (targetTableColumnMap.ContainsKey(sourceColumnName))
+                    if (targetTableColumnMap.Values.Any(item => item.ReplacementText.Equals(newColumnName)))
+                    {
+                        if (warnDuplicateTargetColumnNames)
                         {
                             OnWarningEvent(
-                                "In file {0}, table {1} has multiple columns with the same source name, {2}",
-                                mapFile.Name, newTableName, sourceColumnName);
-
-                            continue;
+                                "In file {0}, table {1} has multiple columns with the same new name, {2}",
+                                mapFile.Name, newTableName, newColumnName);
                         }
-
-                        if (targetTableColumnMap.Values.Any(item => item.ReplacementText.Equals(newColumnName)))
-                        {
-                            if (warnDuplicateTargetColumnNames)
-                            {
-                                OnWarningEvent(
-                                    "In file {0}, table {1} has multiple columns with the same new name, {2}",
-                                    mapFile.Name, newTableName, newColumnName);
-                            }
-                        }
-
-                        var columnNameReplacer = new WordReplacer(sourceColumnName, newColumnName);
-                        targetTableColumnMap.Add(sourceColumnName, columnNameReplacer);
                     }
+
+                    var columnNameReplacer = new WordReplacer(sourceColumnName, newColumnName);
+                    targetTableColumnMap.Add(sourceColumnName, columnNameReplacer);
                 }
 
                 return true;
@@ -148,65 +147,64 @@ namespace TableColumnNameMapContainer
 
             try
             {
-                using (var reader = new StreamReader(new FileStream(mapFile.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+                using var reader = new StreamReader(new FileStream(mapFile.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+
+                while (!reader.EndOfStream)
                 {
-                    while (!reader.EndOfStream)
+                    var dataLine = reader.ReadLine();
+                    linesRead++;
+
+                    if (string.IsNullOrWhiteSpace(dataLine))
+                        continue;
+
+                    var lineParts = dataLine.Split('\t');
+
+                    if (lineParts.Length < 3)
+                        continue;
+
+                    if (linesRead == 1 &&
+                        lineParts[0].Equals("SourceTableName", StringComparison.OrdinalIgnoreCase) &&
+                        lineParts[1].Equals("SourceColumnName", StringComparison.OrdinalIgnoreCase))
                     {
-                        var dataLine = reader.ReadLine();
-                        linesRead++;
-
-                        if (string.IsNullOrWhiteSpace(dataLine))
-                            continue;
-
-                        var lineParts = dataLine.Split('\t');
-
-                        if (lineParts.Length < 3)
-                            continue;
-
-                        if (linesRead == 1 &&
-                            lineParts[0].Equals("SourceTableName", StringComparison.OrdinalIgnoreCase) &&
-                            lineParts[1].Equals("SourceColumnName", StringComparison.OrdinalIgnoreCase))
-                        {
-                            // Header line; skip it
-                            continue;
-                        }
-
-                        var sourceTableName = lineParts[0];
-                        var sourceColumnName = lineParts[1];
-                        var newColumnName = PossiblyUnquote(lineParts[2]);
-
-                        // Look for the table in tableNameMap
-                        if (!tableNameMap.TryGetValue(sourceTableName, out var replacer))
-                        {
-                            if (missingTablesWarned.Contains(sourceTableName))
-                                continue;
-
-                            OnWarningEvent(
-                                "Table {0} not found in tableNameMap; ignoring column map info for column {1}",
-                                sourceTableName, sourceColumnName);
-
-                            missingTablesWarned.Add(sourceTableName);
-                            continue;
-                        }
-
-                        var newTableName = PossiblyUnquote(replacer.ReplacementText);
-
-                        if (!columnNameMap.TryGetValue(newTableName, out var targetTableColumnMap))
-                        {
-                            targetTableColumnMap = new Dictionary<string, WordReplacer>(StringComparer.OrdinalIgnoreCase);
-                            columnNameMap.Add(newTableName, targetTableColumnMap);
-                        }
-
-                        if (targetTableColumnMap.ContainsKey(sourceColumnName))
-                        {
-                            // The column rename map has already been defined; this is OK
-                            OnDebugEvent("Column mapping already defined for {0} in table {1}", sourceColumnName, sourceTableName);
-                            continue;
-                        }
-
-                        var columnNameReplacer = new WordReplacer(sourceColumnName, newColumnName);
-                        targetTableColumnMap.Add(sourceColumnName, columnNameReplacer);
+                        // Header line; skip it
+                        continue;
                     }
+
+                    var sourceTableName = lineParts[0];
+                    var sourceColumnName = lineParts[1];
+                    var newColumnName = PossiblyUnquote(lineParts[2]);
+
+                    // Look for the table in tableNameMap
+                    if (!tableNameMap.TryGetValue(sourceTableName, out var replacer))
+                    {
+                        if (missingTablesWarned.Contains(sourceTableName))
+                            continue;
+
+                        OnWarningEvent(
+                            "Table {0} not found in tableNameMap; ignoring column map info for column {1}",
+                            sourceTableName, sourceColumnName);
+
+                        missingTablesWarned.Add(sourceTableName);
+                        continue;
+                    }
+
+                    var newTableName = PossiblyUnquote(replacer.ReplacementText);
+
+                    if (!columnNameMap.TryGetValue(newTableName, out var targetTableColumnMap))
+                    {
+                        targetTableColumnMap = new Dictionary<string, WordReplacer>(StringComparer.OrdinalIgnoreCase);
+                        columnNameMap.Add(newTableName, targetTableColumnMap);
+                    }
+
+                    if (targetTableColumnMap.ContainsKey(sourceColumnName))
+                    {
+                        // The column rename map has already been defined; this is OK
+                        OnDebugEvent("Column mapping already defined for {0} in table {1}", sourceColumnName, sourceTableName);
+                        continue;
+                    }
+
+                    var columnNameReplacer = new WordReplacer(sourceColumnName, newColumnName);
+                    targetTableColumnMap.Add(sourceColumnName, columnNameReplacer);
                 }
 
                 return true;
